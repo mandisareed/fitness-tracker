a4b7f3c67fd5d6209aad527444231175bc315a3d
Mandisa Reed <mandisa.reed@gmail.com>
2020-07-31 10:01:33 -0700

    added server.js file to establish connection

---
+++ server.js
@@ -0,0 +1,28 @@
+//to
+
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 8080;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb:/"
+
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+});
+
+//create an app
+const app = express();
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+//create a single hello world route
+app.get("*", (req, res) => {
+    res.send(<h1>Hello, World!</h1>);
+});
+
+//listen
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
