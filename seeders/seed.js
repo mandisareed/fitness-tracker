3c7af18c159a1ec303151d7fcdb508c440ed973d
Mandisa Reed <mandisa.reed@gmail.com>
2020-08-03 18:27:25 -0700

    update seeds script with mongodb info for heroku deployment

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,8 +1,10 @@
 let mongoose = require("mongoose");
 let db = require("../models");
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+
 
 //db name is workout, as defined by the connection here:
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
